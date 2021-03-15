9a637527d6387e0a78f1af7a93796d95d73c799e
Law Rui Xi <lawruixi@gmail.com>
2021-03-15 14:52:57 +0800

    added botched lab 10 speedrun things

--- server.py
+++ server.py
@@ -7,5 +7,21 @@ app = flask.Flask(__name__);
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html');
+
+@app.route('/<user>')
+def welcome(user):
+    return render_template('welcome.html', user=user);
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html');
+
+@app.route('/members')
+def members():
+    return render_template('members.html');
+
 if __name__ == '__main__':
     app.run();
