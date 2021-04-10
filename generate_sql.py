8bb1b8155ceba8d22ef53b611101e627423ec3f4
Law Rui Xi <lawruixi@gmail.com>
2021-04-10 17:37:51 +0800

    wrote code to write code, updated sql garbage file

---
+++ generate_sql.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import datetime;
+import random;
+
+random_year = random.randint(2020, 2021);
+random_month = random.randint(1, 12);
+random_day = 0;
+thirty_one_day_months = [1, 3, 5, 7, 8, 10, 12]
+
+while(True):
+    random_day = random.randint(1, 31);
+    if(random_month in thirty_one_day_months):
+        break
+    if(random_day > 28 and random_month == 2): #Screw leap years
+        continue
+    if(random_day == 31):
+        continue
+    break
+
+random_hour = random.randint(0, 24);
+random_minute = random.randint(0, 60);
+random_second = random.randint(0, 60);
+
+random_time = f"{random_year}-{random_month:02}-{random_day:02} {random_hour:02}:{random_minute:02}:{random_second:02}"
+
+id = input().strip()
+title = input().strip();
+content = input().strip();
+
+null = "NULL";
+
+popularity = random.randint(1, 100);
+ratio = random.randint(60, 90)
+if(popularity <= 50):
+    views = random.randint(0, 10);
+elif popularity <= 75:
+    views = random.randint(0, 100);
+elif popularity <= 88:
+    views = random.randint(0, 1000);
+else:
+    views = random.randint(0, 10000);
+
+likes = ratio * views // 100;
+
+
+users = ["admin", "willsuit", "cookie_destroyer", "cow_lover94", "tomminute", "Theseus", "qwerty", "hello", "big_dinosaur11", "Angel_of_Death_9999", "aquaman", "alphabet_king", "tee_sword", "CrispyChips", "P1ZzA", "seedyguy", "RedLavender", "nether_dragon", "symphony", "ouroboros"]
+
+random_user = random.choice(users);
+
+print(f"INSERT INTO post VALUES ({id}, \"{random_time}\", \"{title}\", \"{content}\", {views}, {likes}, \"{random_user}\")")
--- sql_garbage.sql
+++ sql_garbage.sql
@@ -196,22 +196,61 @@ INSERT INTO interest_group VALUES ("Stories", "For writers wishing to try their 
 INSERT INTO interest_group VALUES ("News", "Get up to date with world happenings!", "1998-06-06");
 INSERT INTO interest_group VALUES ("Food", "For all the foodies of the world.", "1992-05-26");
 INSERT INTO interest_group VALUES ("Art", "The world would never be the same without art.", "1992-05-26");
+INSERT INTO interest_group VALUES ("Math", "Mathemagic", "0000-01-01");
 INSERT INTO interest_group VALUES ("Science", "From ashes to ashes, from black holes to atoms", "1999-02-01");
 INSERT INTO interest_group VALUES ("Books", "Talk about your favourite books here!", "2003-07-01");
 INSERT INTO interest_group VALUES ("Movies", "Blockbuster films!", "2016-10-13");
 INSERT INTO interest_group VALUES ("Mine Art II", "The sequel to the best mining simulator game!", "2013-04-29");
+INSERT INTO interest_group VALUES ("Birds", "Cute avian appreciators!", "1982-07-08");
+INSERT INTO interest_group VALUES ("Announcements", "Important announcement regarding the social media Giv.", "2021-04-11");
+INSERT INTO interest_group VALUES ("Programming", "while(true){eat(); sleep();}", "1949-03-13");
+INSERT INTO interest_group VALUES ("Puzzles", "For brilliant young minds!", "1767-12-06");
 
  -- DELETE FROM post;
-INSERT INTO post VALUES (1, "2020-09-09", "POG2020", NULL, 1200, 200, "willsuit");
-INSERT INTO post VALUES (2, "2020-12-28", "Moooooooooooo", NULL, 3729, 1226, "cow_lover94");
-INSERT INTO post VALUES (3, "2020-06-18", "Cookie Monster!", NULL, 487, 282, "cookie_destroyer");
-INSERT INTO post VALUES (4, "2021-01-16", "Athens is the best place", NULL, 70, 69, "Theseus");
-INSERT INTO post VALUES (5, "2021-10-10", "Bought a new keyboard today \:D", NULL, 96, 3, "qwerty");
-INSERT INTO post VALUES (6, "2021-03-21", "Hello World!", NULL, 1, 1, "hello");
-INSERT INTO post VALUES (7, "2021-02-22", "I think I'm extinct...", NULL, 649, 248, "big_dinosaur11");
-INSERT INTO post VALUES (8, "2021-08-13", "Hell > Heaven", NULL, 231, -84, "Angel_of_Death_9999");
-INSERT INTO post VALUES (9, "2021-05-18", "Stay hydrated, folks!", NULL, 9313, 8313, "aquaman");
-INSERT INTO post VALUES (10, "2021-08-10", "ABCDEFEGHIJKLMNOPQRSTUVWXYZ", NULL, 679, 275, "alphabet_king");
+INSERT INTO post VALUES (1, "2014-12-15 14:31:56", "POG2020", "pog", 1200, 200, "willsuit");
+INSERT INTO post VALUES (2, "2020-03-28 07:50:37", "The new update is coming out!", "It drops in one week, folks!", 1200, 200, "willsuit");
+INSERT INTO post VALUES (3, "2010-10-22 02:12:42", "Best game ever!\", "I've been playing this game for 11 years, and I still love it", 355, 342, "P1ZzA")
+INSERT INTO post VALUES (4, "2016-04-29 09:29:24", "How to get started?", "I just bought the game, and I'm not sure how to play it...", 945, 686, "tee_sword")
+INSERT INTO post VALUES (5, "2016-04-30 06:38:52", "Anyone up for a challenge?", "I just beat the game and got first place for the PvP world competition", 903, 543, "tee_sword")
+INSERT INTO post VALUES (6, "2012-02-18 24:03:50", "Lost one week of progress...", "Just got slain by an undead kid. I am never playing this game again :(", 898, 811, "P1ZzA")
+
+INSERT INTO post VALUES (7, "2020-12-28 15:34:41", "Moooooooooooo", "mooooooooooooooo", 3729, 1226, "cow_lover94")
+INSERT INTO post VALUES (8, "2021-01-16 20:48:25", "Athens is the best place", NULL, 70, 69, "Theseus");
+INSERT INTO post VALUES (9, "2021-10-10 14:24:36", "Bought a new keyboard today \:D", NULL, 96, 3, "qwerty");
+INSERT INTO post VALUES (10, "2021-02-22 11:32:15", "I think I'm extinct...", NULL, 649, 248, "big_dinosaur11");
+INSERT INTO post VALUES (11, "2021-08-10 17:30:20", "ABCDEFEGHIJKLMNOPQRSTUVWXYZ", NULL, 679, 275, "alphabet_king");
+INSERT INTO post VALUES (21, "2021-06-23 21:55:30", "I just bought my first bag of chips", "I can't believe I've never tried them before", 3314, 2083, "CrispyChips");
+INSERT INTO post VALUES (22, "2020-04-22 07:52:47", "I just beat the final boss of Dragon Knight!", "I've spent sixty hours on this game, genuinely a masterpiece. Goodbye, Dragon Knight", 8125, 4021, "nether_dragon")
+INSERT INTO post VALUES (23, "2020-04-04 12:40:50", "Does anyone else like plants?", "i spend so long just staring at trees all day...", 841, 585, "seedyguy")
+INSERT INTO post VALUES (24, "2021-07-17 10:58:33", "I like someone, how do I tell them?", "please help i need help", 808, 735, "ouroboros")
+INSERT INTO post VALUES (25, "2021-10-23 13:15:30", "Bored...", "Does anyone want to start a conversation? We can just talk about anything", 74, 23, "hello")
+
+INSERT INTO post VALUES (12, "2020-05-04 16:13:55", "Hell > Heaven", "title", 231, -84, "Angel_of_Death_9999");
+INSERT INTO post VALUES (13, "2020-06-14 04:31:56", "What did one programmer say to the other?", "You're cyber boolean me", 946, 857, "P1ZzA");
+INSERT INTO post VALUES (14, "2020-05-25 14:47:22", "I was reading a book about an immortal dinosaur.", "It was impossible to put down.", 426, 178, "big_dinosaur11");
+INSERT INTO post VALUES (15, "2020-11-15 09:52:05", "A proton walked into a bar...", "and decided to walk back out because it was positive", 843, 137, "big_dinosaur11");
+
+INSERT INTO post VALUES (16, "2021-08-19 23:55:34", "Cookie Monster!", "My new album, please listen to it!", 350, 61, "cookie_destroyer");
+INSERT INTO post VALUES (17, "2021-12-07 05:23:20", "Check out this new song!", "I Recognize You by Melody Knight", 348, 217, "RedLavender");
+INSERT INTO post VALUES (18, "2021-10-29 10:46:50", "I need inspiration...", "composing a song is hard", 937, 280, "nether_dragon");
+INSERT INTO post VALUES (19, "2021-12-27 03:22:57", "music is life", "without music life would be boring", 747, 213, "qwerty");
+INSERT INTO post VALUES (20, "2021-01-04 13:37:08", "Where can I find good songs?", "I want to listen to good songs while swimming, but I don't know where to find them...", 512, 313, "aquaman");
+
+INSERT INTO post VALUES (7, "2021-03-21", "Hello World!", NULL, 1, 1, "hello");
+
+INSERT INTO post VALUES (10, "2021-05-18 23:53:00", "Stay hydrated, folks!", NULL, 9313, 8313, "aquaman");
+INSERT INTO post VALUES (26, "2020-01-05 20:07:39", "How do I resolve this error?", "Help I keep getting some error about my web cookies not existing", 9576, 4585, "CrispyChips")
+INSERT INTO post VALUES (27, "2020-01-16 10:49:46", "How do you plan your space more efficiently?", "I am having many space concerns in my house, I'm not really sure how to deal with this. I think I just have too many things", 32, 25, "Theseus")
+INSERT INTO post VALUES (28, "2021-10-14 05:34:46", "I'm not sure how to deal with loneliness...", "I'm stuck in my home country and can't travel due to government restrictions, and I can't see my friends :(", 98, 57, "nether_dragon")
+INSERT INTO post VALUES (29, "2020-06-14 24:23:19", "how to learn more skills?", "i want to get a better job, but i don't know where i can find places to learn more things", 8773, 6579, "qwerty")
+INSERT INTO post VALUES (30, "2020-10-02 01:20:09", "If you ever feel sad, talk to somebody", "It's a small thing, but it really helps :)", 747, 641, "cow_lover94")
+
+INSERT INTO post VALUES (31, "2021-04-10 17:25:26", "Giv official release tomorrow!", "Finally, giv is being released officially tomorrow! Stay tuned, folks!", 23, 15, "admin")
+INSERT INTO post VALUES (32, "2021-04-10 11:11:15", "giv's changelog!", "To look through giv's changelog, you can visit our official github page :D", 2, 1, "admin")
+
+INSERT INTO post VALUES (33, "2021-03-01 20:33:48", "Once upon a time...", "There was a prince. The prince was really nice. He would help anyone he met. Unfortunately, one day he met an evil man. Oh no! The email man was drowning, but the prince could not let him be. He jumped into the water and saved this evil man. The evil man recognized him, and cast a spell on him, causing him to fall asleep. Only those of the purest heart could save him! A princess came along and saved him though, so it became a win-win scenario. Without the evil man he would never have met his true love. The end.", 48, 40, "CrispyChips")
+INSERT INTO post VALUES (34, "2020-02-22 19:26:45", "So, there was an apple.", "The mystical apple loved conversations. But it did not like Orange. So, when Apple met Orange, he would intentionally exclude Orange from conversations. But Orange never understood why. The poor thing! Orange eventually decided to confront Apple, who revealed he did not like Orange because Orange always spent time with Banana. It turns out Apple liked Banana and was jealous! However, Apple apologized for his behaviour towards Orange, who also made up with Apple. Eventually, the three of them became best friends. The end!", 1, 0, "cow_lover94")
+INSERT INTO post VALUES (35, "2021-04-10 17:36:43", "A student was doing his CS Project...", "He spent so long on it. He was doing it for so long, he started to resent his computer. Every time he sat down and closed his eyes, all he could see was characters, SQL SELECT statements, poorly-formatted HTML code swimming before his eyes. He wanted to give up all the time, but he could not. For the deadline drew ever closer. It didn't help that he had work from other subjects to complete too. He worked day and night, day and night. The hours ticked by. He spent so long in his room, his parents started to worry something was wrong. Finally, when it seemed all hope was lost, he finally managed to finish it on the last minute on the final day. But just as he was about to submit it, the most tragic event imaginable happeEveryone was trying to submit their own project at the same time! Microsoft Teams was down, and the student was in despair as his work would never be completed.", 1, 0, "ouroboros")
 
  -- DELETE FROM posting_info;
 INSERT INTO posting_info VALUES (1, "Mine Art", "willsuit");
